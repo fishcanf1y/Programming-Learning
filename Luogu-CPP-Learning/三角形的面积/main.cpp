0236c0039fcea60010ce101dfee6402b1ff294ab
Hailin Zheng <iszhenghailin@163.com>
2025-03-15 08:38:12 +0800

    Update main.cpp

--- Luogu-CPP-Learning/三角形的面积/main.cpp
+++ Luogu-CPP-Learning/三角形的面积/main.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+double a,b,c;
+double ans;
+int main(){
+    std::cin>>a>>b>>c;
+    double p = 0.5*(a+b+c);
+    double ans= sqrt(p*(p-a)*(p-b)*(p-c));
+    std::cout<<ans<<endl;
+    return 0;
+}
