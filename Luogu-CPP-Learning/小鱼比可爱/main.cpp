4930e48fde1f90d726c112c8bf12da4f18122652
Hailin Zheng <iszhenghailin@163.com>
2025-03-15 08:40:03 +0800

    Create main.cpp

---
+++ Luogu-CPP-Learning/小鱼比可爱/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int a[101],b[101],n;
+int main(){
+    std::cin>>n;
+    for(int i=1;i<=n;i++)
+        cin>>a[i];
+    for(int i=1;i<=n;i++)
+        for(int j=i;j>=1;j--){
+            if(a[j]<a[i])
+            b[i]++;
+        }
+    for(int i=1;i<=n;i++) 
+        cout<<b[i]<<" ";
+    return 0;
+}
