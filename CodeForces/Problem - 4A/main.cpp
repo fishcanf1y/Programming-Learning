1012d41a9960ad238d71660ca259701827f554c9
p4y1oad <iszhenghailin@163.com>
2025-05-14 18:53:46 +0800

    Create main.cpp

---
+++ CodeForces/Problem - 4A/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int w;
+	//cin >> w;
+	scanf("%d",&w);
+	if (w > 2 && w % 2 == 0) {
+		printf("YES");
+	} else {
+		printf("NO");
+	}
+	return 0;
+}
